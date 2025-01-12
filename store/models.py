f91049e901f8eb5845af73d143cad65d96f82b11
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-12 23:00:40 +0330

    add models for db

--- store/models.py
+++ store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    descriptions = models.TextField()
+    top_product = models.ForeignKey('product', on_delete=models.PROTECT, blank=True, null=True)
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image_1 = models.ImageField(upload_to='media/', blank=True)
+    image_2 = models.ImageField(upload_to='media/', blank=True)
+    image_3 = models.ImageField(upload_to='media/')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    available_colors = models.ManyToManyField('Color', blank=True)
+    inventory = models.PositiveIntegerField()
+    descriptions = models.TextField()
+    price = models.PositiveSmallIntegerField()
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=7)
+
+
+
+
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def category_view(request):
+
