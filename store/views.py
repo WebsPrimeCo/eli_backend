7760932e593e15ce8185d0da1ba531b191fa6ee7
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-29 17:20:29 +0330

    add category view

--- store/views.py
+++ store/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from . import models
+from . import serializers
+
+
+@api_view(['GET'])
 def category_view(request):
+    category_queryset = models.Category.objects.all()
+    serializer = serializers.CategorySerializer(data= category_queryset)
+    return Response(serializer)
 
