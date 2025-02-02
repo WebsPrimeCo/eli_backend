df82744284d9f4e3e9019b4bf09db26f4740edbc
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-02 15:22:09 +0330

    change product detail from func to cbv

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('category/', views.CategoryList.as_view(), name= 'category_list'),
     path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category_detail'),
     path('product/', views.ProductList.as_view(), name='product_list'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail')
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product_detail')
 ]
