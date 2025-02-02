ddd42c58b6b991681deb8ff7e903dde39d4b88ea
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-02 15:21:34 +0330

    change productlist from func to cbv

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('category/', views.CategoryList.as_view(), name= 'category_list'),
-    path('category/<int:pk>/', views.category_detail, name='category_detail'),
-    path('product/', views.product_list, name='product_list'),
+    path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category_detail'),
+    path('product/', views.ProductList.as_view(), name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail')
 ]
--- store/views.py
+++ store/views.py
@@ -44,33 +44,54 @@ class CategoryDetail(APIView):
 #         return Response('Category were deleted' ,status=status.HTTP_202_ACCEPTED)
         
 
+class ProductList(ListCreateAPIView):
+    serializer_class = serializers.ProductSerializer
+    queryset = models.Product.objects.select_related('category').prefetch_related('available_colors', 'available_size').all()
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
-        queryset = models.Product.objects.select_related('category').prefetch_related('available_colors', 'available_size').all()
-        serializer = serializers.ProductSerializer(queryset, many=True)
+class ProductDetail(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(models.Product, pk=pk)
+        serializer =serializers.ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = serializers.ProductSerializer(data= request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-@api_view(['GET', 'PATCH', 'DELETE'])
-def product_detail(request, pk):
-    product = get_object_or_404(models.Product, pk=pk)
-
-    if request.method == 'GET':
-        serializer = serializers.ProductSerializer(product)
-        return Response(serializer.data)
-    elif request.method == 'PATCH':
-        serializer = serializers.ProductSerializer(data= request.data)
+    def patch(self, request, pk):
+        product = get_object_or_404(models.Product, pk=pk)
+        serializer = serializers.ProductSerializer(product ,data= request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request, pk):
+        product = get_object_or_404(models.Product, pk=pk)
         product.delete()
         return Response(status=status.HTTP_200_OK)
+
+# @api_view(['GET', 'POST'])
+# def product_list(request):
+#     if request.method == 'GET':
+#         queryset = models.Product.objects.select_related('category').prefetch_related('available_colors', 'available_size').all()
+#         serializer = serializers.ProductSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = serializers.ProductSerializer(data= request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+# @api_view(['GET', 'PATCH', 'DELETE'])
+# def product_detail(request, pk):
+#     product = get_object_or_404(models.Product, pk=pk)
+
+#     if request.method == 'GET':
+#         serializer = serializers.ProductSerializer(product)
+#         return Response(serializer.data)
+#     elif request.method == 'PATCH':
+#         serializer = serializers.ProductSerializer(data= request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     elif request.method == 'DELETE':
+#         product.delete()
+#         return Response(status=status.HTTP_200_OK)
     
     
