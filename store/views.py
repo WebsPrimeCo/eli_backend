9c56ad90e8d079de84e62a7601f5f6db69a972ab
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-02-04 16:23:10 +0330

    add paginations

--- store/views.py
+++ store/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import get_object_or_404, render
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListCreateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+
 
 from . import models
 from . import serializers
-
+from .pagination import DefaultPaginations
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = serializers.CategorySerializer
@@ -16,6 +15,11 @@ class CategoryViewSet(ModelViewSet):
 class ProductViewSet(ModelViewSet):
     serializer_class = serializers.ProductSerializer
     queryset = models.Product.objects.select_related('category').prefetch_related('available_colors', 'available_size').all()
+    filter_backends = [SearchFilter,DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['category_id']
+    ordering_fields = ['title', 'price']
+    search_fields = ['title',]
+    pagination_class = DefaultPaginations
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -31,4 +35,4 @@ class CommentViewSet(ModelViewSet):
         return {
             'request':self.request,
             'product_pk': self.kwargs['product_pk'],
-                } 
+                }
